26ff70fe798ef77995789a74cd16176477e669c6
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-03-28 09:30:18 +0500

    Program file description set

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Name: Ahmed Affaan                                  
+Title: main.py
+Folder: DADSA                            
+Date: 10/03/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description: Assignment
+Student ID: S1802035                                       
+Note: Uncomment codes to execute and comment them when not in use.                         
+"""
+
+# PROGRAM START.
+
+
+
+# PROGRAM END.
